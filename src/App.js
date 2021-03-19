7c279d5db36d86dd3d5a24999e93c25bb020a08c
mofath <mohamed.elgaedi@gmail.com>
2021-03-19 11:41:28 +0200

    Test App component rendered without error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-const App = () => {
-  return <div className="App">app</div>;
-};
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" data-test="component-app">
+        app
+      </div>
+    );
+  }
+}
 
 export default App;

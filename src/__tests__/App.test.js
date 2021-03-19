2945e19dbdd4d3a1da4cb9251e0fa30c4cdbc8da
mofath <mohamed.elgaedi@gmail.com>
2021-03-19 14:32:49 +0200

    Restructure project

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-import App from "./App";
+import App from "../App";
 
 Enzyme.configure({ adapter: new Adapter() });
 

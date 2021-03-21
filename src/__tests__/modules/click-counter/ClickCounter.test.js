06cd7311eadc0c5f4d3c95981dd33f236ea336dc
mofath <mohamed.elgaedi@gmail.com>
2021-03-21 03:20:19 +0200

    Restructure project

--- src/__tests__/modules/click-counter/ClickCounter.test.js
+++ src/__tests__/modules/click-counter/ClickCounter.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-import ClickCounter from "../components/ClickCounter";
+import ClickCounter from "../../../modules/click-counter/ClickCounter";
 
 Enzyme.configure({ adapter: new Adapter() });
 

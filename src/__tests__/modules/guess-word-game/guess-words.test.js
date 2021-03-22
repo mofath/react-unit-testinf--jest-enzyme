ac822b1ce30763aae54117a33cbfdb00007fd189
mofath <mohamed.elgaedi@gmail.com>
2021-03-22 23:30:12 +0200

    Add guessed-words component and tests

---
+++ src/__tests__/modules/guess-word-game/guess-words.test.js
@@ -0,0 +1,56 @@
+import { setup, findByTestAttribute, checkProps } from "../../../testUtils";
+
+import GuessWords from "../../../modules/guess-word-game/guess-words";
+
+const defaultProps = {
+  guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }],
+};
+
+test("does not throw warning with expected props", () => {
+  const props = {};
+  const setupProps = { ...defaultProps, ...props };
+  setup(GuessWords, setupProps);
+  checkProps(GuessWords, defaultProps);
+});
+
+describe("if there're no words guessed", () => {
+  let wrapper = null;
+
+  beforeEach(() => (wrapper = setup(GuessWords, { guessedWords: [] })));
+
+  test("renders without errors", () => {
+    const component = findByTestAttribute(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+
+  test("renders instructions to guess a word", () => {
+    const instructions = findByTestAttribute(wrapper, "guess-instructions");
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+
+describe("if there are words guessed", () => {
+  let wrapper;
+  let guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "party", letterMatchCount: 5 },
+  ];
+
+  beforeEach(() => (wrapper = setup(GuessWords, { guessedWords })));
+
+  test("renders without errors", () => {
+    const component = findByTestAttribute(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+
+  test('renders "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttribute(wrapper, "guessed-words");
+    expect(guessedWordsNode.length).toBe(1);
+  });
+
+  test("correct number of guessed words", () => {
+    const guessedWordNode = findByTestAttribute(wrapper, "guessed-word");
+    expect(guessedWordNode.length).toBe(guessedWords.length);
+  });
+});

d49d303dc609b271231e58748e9521d5ef08e4db
mofath <mohamed.elgaedi@gmail.com>
2021-03-27 00:07:57 +0200

    Do Acceptance test for GuessWords game module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import ClickCounter from './modules/click-counter/click-counter'
+import GuessWords from "./modules/guess-word-game/guess-words";
+import Congrats from "./modules/guess-word-game/congrats";
 
 class App extends Component {
   render() {
     return (
       <div className="App" data-test="component-app">
-        <h1>App</h1>
-        <ClickCounter />
+        <h1>Jotto</h1>
+        <Congrats success={false} />
+        <GuessWords
+          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+        />
       </div>
     );
   }

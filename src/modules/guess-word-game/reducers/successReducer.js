700d219b05f2da3144b8282ae828948586ccf603
mofath <mohamed.elgaedi@gmail.com>
2021-03-27 01:58:21 +0200

    [guress-word-game]: add successReducer and test

--- src/modules/guess-word-game/reducers/successReducer.js
+++ src/modules/guess-word-game/reducers/successReducer.js
@@ -1,3 +1,10 @@
-export default function successReducer(state, action) {
-  return null;
+import { actionTypes } from "../actions";
+
+export function successReducer(state = false, action) {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 }

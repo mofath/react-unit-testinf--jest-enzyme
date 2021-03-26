17eea96729da8825aef05e49eab021f0f96c4c78
mofath <mohamed.elgaedi@gmail.com>
2021-03-27 01:03:26 +0200

    [guress-word-game]: set up redux

--- src/modules/guess-word-game/index.jsx
+++ src/modules/guess-word-game/index.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import GuessWords from "./guess-words";
 import Congrats from "./congrats";
+import { Provider } from "react-redux";
+import store from "./configStore";
 
 class GuessWordGame extends Component {
   render() {
     return (
-      <div className="App" data-test="">
-        <h1>Jotto</h1>
-        <Congrats success={false} />
-        <GuessWords
-          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-        />
-      </div>
+      <Provider store={store}>
+        <div className="App" data-test="">
+          <h1>Jotto</h1>
+          <Congrats success={false} />
+          <GuessWords
+            guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+          />
+        </div>
+      </Provider>
     );
   }
 }
---
+++ src/modules/guess-word-game/reducers/successReducer.js
@@ -0,0 +1,3 @@
+export default function successReducer(state, action) {
+  return null;
+}

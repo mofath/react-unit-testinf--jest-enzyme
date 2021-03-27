08d6b4258e7a950d796f8bd3eade0138f4b2ccdc
mofath <mohamed.elgaedi@gmail.com>
2021-03-28 01:31:19 +0200

    [guress-word-game]: update actions

--- src/modules/guess-word-game/reducers/index.js
+++ src/modules/guess-word-game/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import { successReducer } from "./successReducer";
+import { guessedWordsReducer } from "./guessedWordsReducer";
+
 
 export default combineReducers({
   success: successReducer,
+  guessedWords: guessedWordsReducer,
 });
+

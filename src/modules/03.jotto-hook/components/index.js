8309afbfad4a47bfeab977b56c322380c2090d98
mofath <mohamed.elgaedi@gmail.com>
2021-03-31 00:25:26 +0200

    code cleanup

--- src/modules/03.jotto-hook/components/index.js
+++ src/modules/03.jotto-hook/components/index.js
@@ -1,5 +1,6 @@
 import Congrats from "./Congrats/congrats";
 import GuessWords from "./GuessWords/GuessWords";
 import Input from "./Input/Input";
+import LanguagePicker from "./LanguagePicker/LanguagePicker";
 
-export { Congrats, GuessWords, Input };
+export { Congrats, GuessWords, Input, LanguagePicker };

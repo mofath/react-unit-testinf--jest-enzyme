6e3d9e6b285d0b1cb0c301237a7a92b7106c73bc
mofath <mohamed.elgaedi@gmail.com>
2021-03-31 00:24:53 +0200

    [jotto-hock]: update hookAction

--- src/modules/03.jotto-hook/actions/hookAction.test.js
+++ src/modules/03.jotto-hook/actions/hookAction.test.js
@@ -1,30 +1,27 @@
 import moxios from "moxios";
-
 import { getSecretWord } from "./hookAction";
 
 describe("moxios test", () => {
-  beforeEach(() => {
-    moxios.install();
-  });
-
-  afterEach(() => {
-    moxios.uninstall();
-  });
+  beforeEach(() => moxios.install());
+  afterEach(() => moxios.uninstall());
 
   test("calls the getSecretWord call on axios response", async () => {
     const secretWord = "party";
 
     moxios.wait(() => {
-      const request = moxios.mostRecent();
+      const request = moxios.requests.mostRecent();
       request.respondWith({
         status: 200,
         response: secretWord,
       });
     });
 
+    // create mock for callback arg
     const mockSetSecretWord = jest.fn();
 
-    // see whether mock calles with right argument
-    // expect(mockSetSecretWord).toHaveBeenCalled(secretWord);
+    await getSecretWord(mockSetSecretWord);
+
+    // see whether mock was run with the correct argument
+    // expect(mockSetSecretWord).toHaveBeenCalledWith(secretWord);
   });
 });
